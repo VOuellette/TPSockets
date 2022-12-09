e4b245f53ff193e195da68acdbb41400e14f842b
manumagre <71728627+manumagre@users.noreply.github.com>
2022-12-09 13:42:05 -0500

    Reception du message du client
    Reception du message du client et test(le whaddup) pour la reception du cote client

--- src/SocketServer/ClientThread.java
+++ src/SocketServer/ClientThread.java
@@ -1,7 +1,6 @@
 package SocketServer;
 
 import java.net.Socket;
-
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
@@ -23,9 +22,10 @@ public class ClientThread extends Thread {
 		try {
 			this.out =  new PrintWriter(this.socket.getOutputStream(), true);
 			this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-			
+			//out.println("whaddup");
+			//TODO envoit de la premiere reponse du serveur contenant la liste des users connectes
 			while ((inputLine = in.readLine()) != null) {
-				out.println(inputLine);
+				System.out.println(inputLine);
 			}			
 		}
 		catch(Exception e) {
